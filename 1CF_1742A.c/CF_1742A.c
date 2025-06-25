648286e529adbfa4ce45f3182ce2ce93af0e6ef1
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-06-25 17:34:09 +0600

    1number problem solution

---
+++ 1CF_1742A.c/CF_1742A.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int h;
+    scanf("%d", &h);
+
+    while (h--) {
+        int a, b, c;
+        scanf("%d %d %d", &a, &b, &c);
+
+        if (a + b == c || a + c == b || b + c == a)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+
+    return 0;
+}
+
