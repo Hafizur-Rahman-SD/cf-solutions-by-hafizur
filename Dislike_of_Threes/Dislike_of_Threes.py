ddc3cbf87154cd899a4dc8eda30107ba0bb8e523
Hafizur-Rahman-SD <hrrony859@gmail.com>
2025-06-28 22:08:54 +0600

    Create Dislike_of_Threes.py

---
+++ Dislike_of_Threes/Dislike_of_Threes.py
@@ -0,0 +1,12 @@
+liked_numbers = []
+i = 1
+while len(liked_numbers) < 1000:
+    if i % 3 != 0 and i % 10 != 3:  # 3 দ্বারা বিভাজ্য নয় এবং শেষ অঙ্ক 3 নয়
+        liked_numbers.append(i)
+    i += 1
+
+# টেস্ট কেস নেওয়া
+t = int(input())
+for _ in range(t):
+    k = int(input())
+    print(liked_numbers[k-1])  # k-তম সংখ্যা প্রিন্ট (0-based ইনডেক্স)
